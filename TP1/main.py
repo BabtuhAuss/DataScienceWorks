f83c49046e87f18594ec6d011f703bd540e80185
Baptiste Aussenac <aussenacb@gmail.com>
2020-09-20 22:28:12 -0400

    add requirements.txt

--- TP1/main.py
+++ TP1/main.py
@@ -1,8 +1,6 @@
 #%%
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 from sklearn import datasets
-from sklearn.decomposition import PCA
 import numpy as np
 
 # import some data to play with
